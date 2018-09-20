b600517d41a0cbc666c2e05efcb1310601573a03
Erin Flint <42758837+erinflint@users.noreply.github.com>
2018-09-19 19:14:10 -0600

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,14 @@
+Erin flint
+
+## Week 4 Responses
+This week we got our first little taste of designing pictures with code. We learned how to make circles, triangles, rectangles, arcs, etc. We also learned how to utilize color in our "drawings".
+This week really helped me get a better idea of the various coordinates that are used within a canvas. I had to develop a pretty good grasp on the coordinates and their corresponding numerical values in order to successfully design my centipede/monster thing.
+The steps I took in completing this week's homework were as follows:
+
+ - create my canvas
+ - design my monster by drawing out my canvas/monster on a sheet of paper (it was easier for me to do this before actually coding in order to familiarize myself with where various parts of my monster would fall on my canvas)
+ - follow along with the course website in order to bring my monster to life
+ - play play play!
+
+ I didn't have many problems this week that I couldn't manage to troubleshoot by myself. I did struggle a little with keeping my x and y coordinates straight in my head but that was teased out with repetition. This week really opened my eyes to merely a fraction of what creative coding can do for the art world. I made a very rudimentary monster and cannot wait to see what I am able to make by the end of this semester.
+ I did not assist anyone in the issues forum this week. I read the issues forum and either did not know how to help the asker or was not experiencing the same issues.
--- sketch.js
+++ sketch.js
@@ -1,11 +1,69 @@
 function setup(){
   //create canvas size 600px wide and 400px high
-  createCanvas( 600, 400 );
-
-//color background yellow
-background( 'yellow' );
+  createCanvas( 450, 700 );
 }
-function draw(){
-//tell the program to write "Hello World" at 50px by 60px
-text( "Hello World", 50, 60 );
+//eyes
+function draw() {
+  background( 'rgb(78, 230, 180)' );
+//head shape
+  fill('pink');
+  ellipse( 150, 125, 100, 100 );
+
+//body
+  fill('yellow');
+  triangle( 100, 175, 200, 175, 150, 250 );
+
+
+//second abdomen
+fill('yellow');
+triangle( 100, 250, 200, 250, 170, 350 );
+
+//third abdomen
+fill('yellow');
+triangle( 110, 360, 220, 350, 200, 450 );
+
+//fourth abdomen
+fill('yellow');
+triangle( 150, 460, 240, 450, 280, 550 );
+//first torso legs
+line( 40, 220, 55, 200 );
+line( 55, 200, 110, 200 );
+line( 190, 200, 240, 200 );
+line( 240, 200, 260, 240 );
+
+//horns
+line( 105, 50, 100, 100 );
+line( 100, 100, 125, 110 );
+line( 190, 50, 190, 100 );
+line( 190, 100, 170, 110 );
+line( 75, 75, 100, 85 );
+line( 190, 75, 215, 65 );
+
+//second torso legs
+line( 40, 320, 50, 290 );
+line( 50, 290, 120, 290 );
+line( 190, 290, 240, 290 );
+line( 240, 290, 260, 320 );
+//third torso legs
+line( 90, 460, 100, 420 );
+line( 100, 420, 140, 400 );
+line( 210, 390, 250, 390 );
+line( 250, 390, 270, 420 );
+//fourth torso legs
+line( 200, 600, 180, 540 );
+line( 180, 540, 200, 500 );
+line( 255, 480, 280, 480 );
+line( 280, 480, 310, 530 );
+//tail
+line( 280, 550, 320, 650 );
+line( 280, 550, 300, 640 );
+line( 280, 550, 280, 630 );
+line( 280, 550, 330, 640 );
+line( 280, 550, 340, 625 );
+//mouth
+fill('red');
+arc( 120, 150, 80, 20, 180, 45 );
+fill('black');
+strokeWeight(5);
+point( 100, 120 );
 }
